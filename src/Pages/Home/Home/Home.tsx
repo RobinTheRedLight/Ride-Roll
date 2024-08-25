6f1cf634e62020a2a64bbe99374d64cd3bfeb30f
RobinTheRedLight <robinmitra789@gmail.com>
2024-08-25 22:21:52 +0600

    Project-initialized

---
+++ src/Pages/Home/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Ride & Roll | Home</title>
+      </Helmet>
+      <h1>This is a Home page</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes.tsx";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
+  </StrictMode>
+);
